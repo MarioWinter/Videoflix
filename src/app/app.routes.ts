da7a364463903233203b55b06b17c49e3bc40778
Marco Angermann <159908924+MarcoAngermann@users.noreply.github.com>
2025-04-08 09:07:47 +0200

    create legal pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: 'bearbeiten', component: ImprintComponent },
+    { path:'impressum', component: PrivacyPolicyComponent },
+];
