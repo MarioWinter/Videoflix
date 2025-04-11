bbff3b67c15687f90bf4ecd778e1c8368f6e0325
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-04-11 12:02:38 +0200

    changed log in button to use it in different pages.

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
+
+  handleLogin() {
+    console.log('Logging in...');
+  }
 
 }
