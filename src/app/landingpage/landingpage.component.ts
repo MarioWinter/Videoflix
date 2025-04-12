7f217a54bae7376e56ed10b040e2f8a04d2ab333
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-04-12 09:49:44 +0200

    changed footer component to use it in different pages

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+  import { Component } from '@angular/core';
+  import { HeaderComponent } from '../header/header.component';
+  import { FooterComponent } from '../footer/footer.component';
 
-@Component({
-  selector: 'app-landingpage',
-  imports: [HeaderComponent],
-  templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
-})
-export class LandingpageComponent {
+  @Component({
+    selector: 'app-landingpage',
+    imports: [HeaderComponent, FooterComponent],
+    templateUrl: './landingpage.component.html',
+    styleUrl: './landingpage.component.scss'
+  })
+  export class LandingpageComponent {
 
-  handleLogin() {
-    console.log('Logging in...');
+    handleLogin() {
+      console.log('Logging in...');
+    }
+
   }
-
-}
