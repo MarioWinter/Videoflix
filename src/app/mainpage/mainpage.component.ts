1085bd726e70c8cd9fe2a95d34a0705d0e82bbad
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-04-16 11:08:17 +0200

    added header and footer into mainpage component

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
 export class MainpageComponent {
+
+  handleLogOut() {
+    console.log('Logging out...');
+  }
 
 }
