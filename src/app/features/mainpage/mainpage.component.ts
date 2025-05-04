943da5f583e7ed4c94498e64f9cdc20b75a1990f
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-05-04 20:57:08 +0200

    refactor: svg assets, dark background collor added to styles.scss, px to rem conversion

--- src/app/features/mainpage/mainpage.component.ts
+++ src/app/features/mainpage/mainpage.component.ts
@@ -24,19 +24,19 @@ export class MainpageComponent {
 	videoSections: VideoSection[] = [
 		{
 			title: "New on Videoflix",
-			images: ["/assets/img/frame1.1.png", "/assets/img/frame1.2.png", "/assets/img/frame1.3.png", "/assets/img/frame1.4.png", "/assets/img/frame1.5.png", "/assets/img/frame1.6.png"],
+			images: ["/assets/img/frame1.1.svg", "/assets/img/frame1.2.svg", "/assets/img/frame1.3.svg", "/assets/img/frame1.4.svg", "/assets/img/frame1.5.svg", "/assets/img/frame1.6.svg"],
 		},
 		{
 			title: "Documentary",
-			images: ["/assets/img/frame1.2.png", "/assets/img/frame1.5.png", "/assets/img/frame1.4.png"],
+			images: ["/assets/img/frame1.2.svg", "/assets/img/frame1.5.svg", "/assets/img/frame1.4.svg"],
 		},
 		{
 			title: "Drama",
-			images: ["/assets/img/frame1.7.png", "/assets/img/frame1.8.png", "/assets/img/frame1.9.png", "/assets/img/frame1.9.1.png"],
+			images: ["/assets/img/frame1.7.svg", "/assets/img/frame1.8.svg", "/assets/img/frame1.9.svg", "/assets/img/frame1.9.1.svg"],
 		},
 		{
 			title: "Romance",
-			images: ["/assets/img/frame1.9.png", "/assets/img/frame1.9.1.png"],
+			images: ["/assets/img/frame1.9.svg", "/assets/img/frame1.9.1.svg"],
 		},
 	];
 }
