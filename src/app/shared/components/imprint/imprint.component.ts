c4122d1091d6cc96160cf66444a865123762cc5a
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-05-07 08:08:30 +0200

    add additional routings in legal pages with goback funktion. switched legal words from german to english

--- src/app/shared/components/imprint/imprint.component.ts
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
-  imports: [ HeaderComponent, FooterComponent ],
+  imports: [ HeaderComponent, FooterComponent, RouterModule ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }
--- src/app/shared/components/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/components/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-privacy-policy',
-  imports: [ HeaderComponent, FooterComponent ],
-  templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+    selector: 'app-privacy-policy',
+    imports: [HeaderComponent, FooterComponent, RouterModule],
+    templateUrl: './privacy-policy.component.html',
+    styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
+    constructor(private location: Location) {}
 
+    goBack() {
+        this.location.back();
+    }
 }
