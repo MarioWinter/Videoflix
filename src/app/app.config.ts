7262f50a93da82bf07d9f026fa220b048c3daba0
Mario Winter <mariowinter.sg@gmail.com>
2025-05-08 20:37:35 +0200

    chore(config): switch to tabs and configure HTTP client with CSRF protection and interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideHttpClient, withXsrfConfiguration, withInterceptorsFromDi } from "@angular/common/http";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)]
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(routes),
+		provideHttpClient(
+			withXsrfConfiguration({
+				cookieName: "csrftoken",
+				headerName: "X-CSRFToken",
+			}),
+			withInterceptorsFromDi()
+		),
+	],
 };
