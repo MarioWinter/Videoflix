5c06e2e63c72fe5429f2347ef3197e3d1918c2fd
Mario Winter <mariowinter.sg@gmail.com>
2025-05-18 12:40:12 +0200

    style(ui): enhance footer and legal pages with responsive layout and route-aware logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { FooterComponent } from "./shared/components/footer/footer.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet, FooterComponent],
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.scss",
+	selector: 'app-root',
+	imports: [RouterOutlet, HeaderComponent, FooterComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-	title = "Videoflix";
+	title = 'Videoflix';
+
+	constructor(private router: Router) {
+		this.router.events
+			.pipe(filter((event) => event instanceof NavigationEnd))
+			.subscribe(() => {
+				this.scrollToTop();
+			});
+	}
+
+	scrollToTop(): void {
+		window.scroll({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	}
 }
--- src/app/shared/components/imprint/imprint.component.ts
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-imprint',
-  imports: [],
-  templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+	selector: 'app-imprint',
+	imports: [RouterModule],
+	templateUrl: './imprint.component.html',
+	styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+	constructor(private location: Location) {}
 
+	goBack() {
+		this.location.back();
+	}
 }
