d03e195c11a4e11c3c042c3c4dbf0e9443e4b1ee
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-05-25 20:13:09 +0200

    corrected superfluous code in footer component from the previous merge. landingpage and app routes formatted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,45 +1,46 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { LandingpageComponent } from './features/landingpage/landingpage.component';
-import { MainpageComponent } from './features/mainpage/mainpage.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { MainpageComponent } from './features/mainpage/mainpage.component';
+import { PreviewComponent } from './features/preview/preview.component';
+import { VideoPlayerComponent } from './features/video-player/video-player.component';
 import { ImprintComponent } from './shared/components/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './shared/components/privacy-policy/privacy-policy.component';
 import { VerifyEmailComponent } from './features/auth/verify-email/verify-email.component';
-import { PreviewComponent } from "./features/preview/preview.component";
-import { VideoPlayerComponent } from "./features/video-player/video-player.component";
 
 export const routes: Routes = [
-	{ 
-    path: 'register', component: RegisterComponent 
-  },
+    {
+        path: '',
+        component: LandingpageComponent,
+    },
 	{
-		path: '',
-		component: LandingpageComponent,
-	},
-	{
-		path: 'mainpage',
-		component: MainpageComponent,
-		canActivate: [AuthGuard],
-	},
-	{
-		path: "preview",
-		component: PreviewComponent,
-	},
-	{
-		path: "video-player",
-		component: VideoPlayerComponent,
-	},
-	{
-		path: "imprint",
-		component: ImprintComponent,
-	},
-	{
-		path: "privacy-policy",
-		component: PrivacyPolicyComponent,
-	},
-	{ 
-    path: 'verify-email', 
-    component: VerifyEmailComponent, 
-  },
+        path: 'register',
+        component: RegisterComponent,
+    },
+    {
+        path: 'mainpage',
+        component: MainpageComponent,
+        canActivate: [AuthGuard], 
+    },
+    {
+        path: 'preview',
+        component: PreviewComponent,
+    },
+    {
+        path: 'video-player',
+        component: VideoPlayerComponent,
+    },
+    {
+        path: 'imprint',
+        component: ImprintComponent,
+    },
+    {
+        path: 'privacy-policy',
+        component: PrivacyPolicyComponent,
+    },
+    {
+        path: 'verify-email',
+        component: VerifyEmailComponent,
+    },
 ];
